91197a91d04e62ec95cae4be31230680aa987398
mario5698 <arancibiagm@gmail.com>
2021-02-17 17:20:26 +0100

    add Acceso_dades classe

---
+++ Inner Ring/Acceso_Dades/Conexion.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Acceso_Dades
+{
+    public abstract class Conexion
+    {
+        private string connectionString;
+        private protected SqlConnection conexion;
+        private protected SqlDataAdapter adaptador;
+        string query;
+        DataSet dts;
+
+        public Conexion()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["SecureCore.Properties.Settings.SecureCoreConnectionString"].ConnectionString;
+        }
+
+        private protected void Conectar(string query)
+        {
+            conexion = new SqlConnection(connectionString);
+
+            try
+            {
+                if (query != null && query != "")
+                {
+                    adaptador = new SqlDataAdapter(query, conexion);
+                    if (conexion.State == ConnectionState.Closed) conexion.Open();
+                }
+            }
+            catch (SqlException)
+            {
+
+            }
+        }
+
+        public DataTable PortarTaula(string tabla)
+        {
+            dts = new DataSet();
+            query = "select * from " + tabla;
+            Conectar(query);
+            adaptador.Fill(dts, tabla);
+            conexion.Close();
+            return dts.Tables[tabla];
+        }
+
+        public void Actualitzar()
+        {
+            conexion.Open();
+            SqlDataAdapter adaptador;
+            adaptador = new SqlDataAdapter(query, conexion);
+            adaptador.RowUpdated += new SqlRowUpdatedEventHandler(OnRowUpdated);
+            SqlCommandBuilder cmdBuilder;
+            cmdBuilder = new SqlCommandBuilder(adaptador);
+            adaptador.Update(dts.Tables[0]);
+            conexion.Close();
+        }
+
+        private void OnRowUpdated(object sender, SqlRowUpdatedEventArgs args)
+        {
+            if (args.Status == UpdateStatus.ErrorsOccurred)
+            {
+                args.Status = UpdateStatus.SkipCurrentRow;
+            }
+        }
+
+        public DataSet PortarPerConsulta(string consulta)
+        {
+            dts = new DataSet();
+            Conectar(consulta);
+            adaptador.Fill(dts);
+            conexion.Close();
+            return dts;
+        }
+        public DataSet PortarPerConsulta(string consulta, string tabla)
+        {
+            dts = new DataSet();
+            Conectar(consulta);
+            adaptador.Fill(dts, tabla);
+            conexion.Close();
+            return dts;
+        }
+
+        public int Executa(string consult)
+        {
+            Conectar(consult);
+            SqlCommand cmd = new SqlCommand(consult, conexion);
+            int registresAfectats = cmd.ExecuteNonQuery();
+            return registresAfectats;
+        }
+
+        public void Store()
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand("Ten Most Expensive Products", conexion);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@Login", "1"));
+            //cmd.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+    }
+}
--- Inner Ring/Inner Ring/Planet.Designer.cs
+++ Inner Ring/Inner Ring/Planet.Designer.cs
@@ -29,22 +29,34 @@ namespace Inner_Ring
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // Home
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(357, 107);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Planet
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(40)))), ((int)(((byte)(50)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.ForeColor = System.Drawing.Color.PaleGreen;
-            this.Name = "Home";
+            this.Name = "Planet";
             this.Text = "Home";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
